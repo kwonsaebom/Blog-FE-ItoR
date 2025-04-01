57cade30a21be40dafff358ad37ead41b21490ea
봄 <saebom@Saebomui-MacBookPro.local>
2025-04-01 22:46:15 +0900

    feat: Header 제작

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,3 +1,29 @@
+import MenuIcon from '@/assets/icons/icon_menu.svg?react'
+import LogoIcon from '@/assets/icons/logo_black.svg?react'
+
+import styled from 'styled-components'
+
 export default function Header() {
-  return <h1>Hello!! Header</h1>
+  return (
+    <StyledHeader>
+      <StyledMenuIcon />
+      <StyledLogoIcon />
+    </StyledHeader>
+  )
 }
+
+const StyledHeader = styled.header`
+  height: 40px;
+  padding: 16px 16px 16px 12px;
+  display: flex;
+  align-items: center;
+`
+const StyledMenuIcon = styled(MenuIcon)`
+  width: 40px;
+  height: 40px;
+`
+
+const StyledLogoIcon = styled(LogoIcon)`
+  width: 77px;
+  height: 28px;
+`
