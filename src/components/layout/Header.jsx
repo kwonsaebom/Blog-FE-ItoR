7e7329c439124928461523a69ef5a8a5b97fd04f
봄 <saebom@Saebomui-MacBookPro.local>
2025-04-05 16:06:17 +0900

    feat: 테일윈드 세팅

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,25 +5,9 @@ import styled from 'styled-components'
 
 export default function Header() {
   return (
-    <StyledHeader>
-      <StyledMenuIcon />
-      <StyledLogoIcon />
-    </StyledHeader>
+    <header class='flex items-center'>
+      <MenuIcon></MenuIcon>
+      <LogoIcon class='w-[77px] h-[28px]'></LogoIcon>
+    </header>
   )
 }
-
-const StyledHeader = styled.header`
-  height: 40px;
-  padding: 16px 16px 16px 12px;
-  display: flex;
-  align-items: center;
-`
-const StyledMenuIcon = styled(MenuIcon)`
-  width: 40px;
-  height: 40px;
-`
-
-const StyledLogoIcon = styled(LogoIcon)`
-  width: 77px;
-  height: 28px;
-`
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,14 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
 import Header from '@components/layout/Header'
-import Footer from '@components/layout/Footer'
 
 export default function layout() {
   return (
     <>
       <Header />
       <Outlet />
-      <Footer />
     </>
   )
 }
