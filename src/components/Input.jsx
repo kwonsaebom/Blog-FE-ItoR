41d5d68b317bf00a61aad820e2a11a8cf9281e10
Saebom <spring180@naver.com>
2025-04-07 02:59:45 +0900

    feat: 로그인 컴포넌트 생성

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,12 @@
-export default function Input({ type, label }) {
+export default function Input({ type, label, isLoginPage = false }) {
   const limit = <p className='text-gray78 text-xs'>* 20글자 이내</p>
   return (
-    <label className='py-3 flex flex-col gap-3'>
-      {label}
+    <label className={`flex flex-col gap-3 ${isLoginPage ? '' : 'my-3'}`}>
+      {isLoginPage ? undefined : label}
       <input
         type={type}
-        placeholder={type === 'password' ? '......' : label}
-        className='py-3 px-4 border border-gray90 rounded-sm placeholder:text-gray78 '
+        placeholder={label} // type === 'password' ? '...' : label
+        className='py-3 px-4 bg-white border border-gray90 rounded-sm placeholder:text-gray78 '
       />
       {label === '닉네임' ? limit : undefined}
     </label>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,35 +1,12 @@
-import LogoIcon from '@/assets/icons/logo_black.svg?react'
-import KakaoIcon from '@/assets/icons/icon_kakao.svg?react'
+import Login from '@components/Login'
+
 export default function SignUpPage() {
   return (
     <>
       <header className='bg-gray96 h-[90px] flex items-center desktop:h-[114px]'>
         <h1 className='py-3 px-4 text-2xl font-medium min-w-[782px] m-auto'>회원가입</h1>
       </header>
-      <section className='py-20 px-12 desktop:max-w-[782px] desktop:py-20 desktop:m-auto desktop:flex desktop:gap-[90px]'>
-        <aside>
-          <LogoIcon className='w-[283px] h-[76px] my-10' />
-          <p className='px-3 py-4 text-gray56 text-sm font-extralight text-center'>
-            You can make anything by writing
-          </p>
-        </aside>
-        <div className='flex flex-col gap-2 desktop:my-10'>
-          <button className='w-[294px] h-[45px] rounded-md bg-point text-white text-sm cursor-pointer desktop:w-[312px]'>
-            이메일로 회원가입
-          </button>
-          <span
-            className='text-gray56 text-xs text-center relative 
-          before:content-[""] before:block before:h-[1px] before:w-[123px] before:bg-gray96 before:absolute before:top-1/2 before:left-3 before:translate-y-1/2
-          after:content-[""] after:block after:h-[1px] after:w-[123px] after:bg-gray96 after:absolute after:top-1/2 after:right-3 after:translate-y-1/2'
-          >
-            또는
-          </span>
-          <button className='w-[294px] h-[45px] rounded-md bg-kakao-bg text-sm cursor-pointer desktop:w-[312px]'>
-            <KakaoIcon className='inline-block mr-1 w-6 h-6 relative top-0.5' />
-            <span>카카오로 회원가입</span>
-          </button>
-        </div>
-      </section>
+      <Login />
     </>
   )
 }
