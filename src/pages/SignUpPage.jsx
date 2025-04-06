28d4c9bbdc77e5b918c9df745601d36cddaf50c8
Saebom <spring180@naver.com>
2025-04-07 00:54:38 +0900

    feat: 회원가입 페이지 반응형 디자인

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -3,18 +3,18 @@ import KakaoIcon from '@/assets/icons/icon_kakao.svg?react'
 export default function SignUpPage() {
   return (
     <>
-      <header className='bg-gray96 h-20 flex items-center'>
-        <h1 className='text-2xl font-medium min-w-[782px] m-auto'>회원가입</h1>
+      <header className='bg-gray96 h-[90px] flex items-center desktop:h-[114px]'>
+        <h1 className='py-3 px-4 text-2xl font-medium min-w-[782px] m-auto'>회원가입</h1>
       </header>
-      <section className='max-w-[782px] py-20 m-auto flex gap-[90px]'>
+      <section className='py-20 px-12 desktop:max-w-[782px] desktop:py-20 desktop:m-auto desktop:flex desktop:gap-[90px]'>
         <aside>
-          <LogoIcon className='w-[283px] h-[76px]' />
-          <p className='px-3 py-4 text-gray56 text-sm font-light text-center'>
+          <LogoIcon className='w-[283px] h-[76px] my-10' />
+          <p className='px-3 py-4 text-gray56 text-sm font-extralight text-center'>
             You can make anything by writing
           </p>
         </aside>
-        <div className='flex flex-col gap-2'>
-          <button className='w-[312px] h-[45px] rounded-md bg-point text-white cursor-pointer'>
+        <div className='flex flex-col gap-2 desktop:my-10'>
+          <button className='w-[294px] h-[45px] rounded-md bg-point text-white text-sm cursor-pointer desktop:w-[312px]'>
             이메일로 회원가입
           </button>
           <span
@@ -24,8 +24,8 @@ export default function SignUpPage() {
           >
             또는
           </span>
-          <button className='w-[312px] h-[45px] rounded-md bg-kakao-bg cursor-pointer'>
-            <KakaoIcon className='inline-block ㅡr-2 w-6 h-6' />
+          <button className='w-[294px] h-[45px] rounded-md bg-kakao-bg text-sm cursor-pointer desktop:w-[312px]'>
+            <KakaoIcon className='inline-block mr-1 w-6 h-6 relative top-0.5' />
             <span>카카오로 회원가입</span>
           </button>
         </div>
