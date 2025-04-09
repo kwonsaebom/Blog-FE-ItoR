f8e6874463e573eecf37b9c95f068440e5ba8996
Saebom <spring180@naver.com>
2025-04-10 02:41:10 +0900

    feat: 조건 별 헤더 수정

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 import useModal from '@hooks/useModal'
 import Modal from '@components/Modal'
@@ -10,6 +10,9 @@ import LogoIcon from '@assets/icons/logo_black.svg?react'
 export default function Header() {
   const { isOpen, open, close } = useModal()
 
+  const pathname = useLocation().pathname
+  const isWritePage = pathname.includes('write')
+
   return (
     <header className='py-4 px-3 border-b border-gray96 flex items-center justify-between'>
       <div className='flex items-center gap-3'>
@@ -20,10 +23,17 @@ export default function Header() {
           <LogoIcon className='w-[67px] h-[28px]' />
         </Link>
       </div>
-      <button className='py-2 px-3 flex items-center gap-1 cursor-pointer'>
-        <WriteIcon className='text-gray56' />
-        <span className='text-gray56 text-sm'>깃로그 쓰기</span>
-      </button>
+      {isWritePage ? (
+        <div className='flex gap-2'>
+          <button className='py-2 px-3 text-negative'>삭제하기</button>
+          <button className='py-2 px-3'>게시하기</button>
+        </div>
+      ) : (
+        <Link to='/write' className='py-2 px-3 flex items-center gap-1 cursor-pointer'>
+          <WriteIcon className='text-gray56' />
+          <span className='text-gray56 text-sm'>깃로그 쓰기</span>
+        </Link>
+      )}
       {isOpen && <Modal isLoginPage={true} onClose={close} />}
     </header>
   )
