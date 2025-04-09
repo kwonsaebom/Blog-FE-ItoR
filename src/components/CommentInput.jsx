27d0b7288eeb058720990314819ccceda370a558
Saebom <spring180@naver.com>
2025-04-10 02:09:59 +0900

    feat: 댓글 작성 컴포넌트 구현

---
+++ src/components/CommentInput.jsx
@@ -0,0 +1,26 @@
+import PostMeta from '@components/PostMeta'
+
+export default function CommentInput({ isLogin = false }) {
+  const renderTextarea = (placeholder, extraClass = '') => (
+    <textarea
+      placeholder={placeholder}
+      className={`h-20 w-full py-3 px-4 rounded-sm text-xs font-light text-gray30 placeholder:text-gray56 resize-none focus:outline-0 ${extraClass}`}
+    />
+  )
+
+  return (
+    <>
+      {isLogin ? (
+        <div className='py-3 px-4 rounded-sm border border-gray90 flex flex-col'>
+          <PostMeta isDetail={false} />
+          {renderTextarea('댓글을 입력하세요.', 'border-b border-gray96 px-0')}
+          <button className='w-fit py-2 px-4 mt-2 border border-gray56 rounded-3xl text-gray56 text-sm self-end'>
+            등록
+          </button>
+        </div>
+      ) : (
+        renderTextarea('로그인을 하고 댓글을 달아보세요!', 'my-3 border border-gray90')
+      )}
+    </>
+  )
+}
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import PostMeta from '@components/PostMeta'
 import PostImage from '@components/PostImage'
 export default function PostItem({ isImg = false }) {
@@ -5,7 +7,7 @@ export default function PostItem({ isImg = false }) {
 
   return (
     <>
-      <section className='flex py-2 border-b border-b-gray96'>
+      <Link to='/post/1' className='flex py-2 border-b border-b-gray96'>
         <div className='py-3 px-4 flex flex-col gap-2 pb-0'>
           <h2 className='font-medium'>16 Title on line</h2>
           <p className='pb-1 text-gray33 text-sm font-light line-clamp-2'>
@@ -16,7 +18,7 @@ export default function PostItem({ isImg = false }) {
           <PostMeta />
         </div>
         {isImg && <PostImage size='small' />}
-      </section>
+      </Link>
     </>
   )
 }
