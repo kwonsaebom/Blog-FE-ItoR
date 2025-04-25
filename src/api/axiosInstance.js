537295fca0760c7bc53f3c232f3fc632672ba8b7
봄 <saebom@Saebomui-MacBookPro.local>
2025-04-25 20:46:30 +0900

    feat: axios interceptor 적용

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -7,4 +7,26 @@ const axiosInstance = axios.create({
   },
 })
 
+// 요청 인터셉터
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+// 응답 인터셉터
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response?.status === 401) {
+      // 토큰 만료, 로그아웃 처리
+      alert('세션이 만료되었습니다.')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  },
+)
+
 export default axiosInstance
